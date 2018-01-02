70e932a3752ce822944a6c00a460cd381c15064e
colinjpaul <colinpaul@gmail.com>
2018-01-02 16:08:45 +0000

    python from home

--- chapter_4/magicians.py
+++ chapter_4/magicians.py
@@ -1,4 +1,4 @@
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
     print(magician.title() + ", that was a great trick!")
-    print("test from work account")
+    print("I can't wait to see your next trick, " + magician.title() + ".\n")
--- chapter_4/numbers.py
+++ chapter_4/numbers.py
@@ -1,3 +1,28 @@
-for value in range(1,5):
-    print(value)
+# numbers = list(range(1, 6))
+# print(numbers)
+#
+# odd_numbers = list(range(1, 20, 2))
+# print(odd_numbers)
+#
+# squares = [value**2 for value in range(1, 11)]
+# print(squares)
+#
+# for value in range(1, 21):
+#     print(value)
+
+# threes = [value*3 for value in range(3, 31, 3)]
+# print(threes)
+
+cubes = [value**3 for value in range(1, 11)]
+print(cubes)
+# numbers = list(range(1, 1000001))
+# for number in numbers:
+#     print(number)
+
+# print(min(numbers))
+# print(max(numbers))
+# print(sum(numbers))
+
+
+
 
