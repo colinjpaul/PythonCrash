184b4182c726ae2f3775156c80c5af826e73011f
Colin Paul <colin.paul@dell.com>
2018-02-09 15:55:26 +0000

    onto chapter 7

---
+++ chapter_7/greeter.py
@@ -0,0 +1,11 @@
+prompt = "if you tell us who you are we can personalise the mesasges you see"
+prompt += "\nWhat is your first name > "
+
+name = input(prompt)
+age = int(input("how old are you > "))
+print("hello, " + name)
+print(age)
+age >= 21
+
+
+
---
+++ chapter_7/parrot.py
@@ -0,0 +1,2 @@
+message = input("tell me something and i'll repeat it > ")
+print(message)
