16116efcac48873d8c62c3f98369efbee060f79b
colinjpaul <colinpaul@gmail.com>
2018-03-03 07:49:03 +0000

    caught_up

--- chapter_8/try_it_yourself/magicians_8_11.py
+++ chapter_8/try_it_yourself/magicians_8_11.py
@@ -7,14 +7,14 @@ def make_great(magician_list, great_magician_list):
     while magician_list:
         great_magician = magician_list.pop()
         great_magician_list.append(great_magician + ' the Great')
+    return great_magician_list
 
 
 magicians = ['mick', 'john', 'tom']
 great_magicians = []
 
-show_magicians(magicians)
-make_great(magicians, great_magicians)
-show_magicians(magicians)
+great_magicians = make_great(magicians[:], great_magicians)
 show_magicians(great_magicians)
+show_magicians(magicians)
 
 
