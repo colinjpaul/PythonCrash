423777439ef8dbef5ef7ac900a1d877376ab6b3e
Colin <Colin_Paul@McAfee.com>
2018-06-07 09:19:04 +0100

    baby steps

--- chapter_9/try_it_yourself/restaurant.py
+++ chapter_9/try_it_yourself/restaurant.py
@@ -3,23 +3,17 @@ class Restaurant():
     def __init__(self, restaurant_name, cuisine_type):
         self.name = restaurant_name
         self.cuisine = cuisine_type
+        self.number_served = 17
 
     def describe_restaurant(self):
         print('Restaurant Name: ' + self.name)
         print('Cuisine Type: ' + self.cuisine)
+        print('Customers Served: ' + str(self.number_served))
 
     def open_restaurant(self):
         print('The Restaurant is now open')
 
 
-restaurant1 = Restaurant('Ashy Pally', 'Mexican')
-restaurant2 = Restaurant('Conna', 'Italian')
-restaurant3 = Restaurant('Dennys', 'Traditional')
-
-restaurant1.describe_restaurant()
-restaurant2.describe_restaurant()
-restaurant3.describe_restaurant()
-
-
-
+restaurant = Restaurant('Conna Cuisine', 'Traditional')
+restaurant.describe_restaurant()
 
