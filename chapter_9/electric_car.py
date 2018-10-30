7eefcce060a1decbf220588b866f41a30c7c43fc
colinjpaul <colinpaul@gmail.com>
2018-10-30 06:43:50 +0000

    updating comment format

--- chapter_9/electric_car.py
+++ chapter_9/electric_car.py
@@ -1,5 +1,5 @@
 class Car():
-    '''A sipmple attempt to represent a car'''
+    """A sipmple attempt to represent a car"""
 
     def __init__(self, make, model, year):
         self.make = make
@@ -8,7 +8,7 @@ class Car():
         self.odometer_reading = 0
 
     def get_descriptive_name(self):
-        '''Return a neatly formatted descriptive name'''
+        """Return a neatly formatted descriptive name"""
         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
         return long_name.title()
 
@@ -16,19 +16,18 @@ class Car():
         print('Cars mileage is ' + str(self.odometer_reading))
 
     def update_odometer(self, mileage):
-        '''
+        """
         Set the odometer reading to a given value
         Reject the change if it attempts to roll back to reading
-        '''
+        """
         if mileage >= self.odometer_reading:
             self.odometer_reading = mileage
         else:
             print("You can't roll back an odometer")
 
     def increment_odometer(self, miles):
-        '''Add the given amount to the odometer reading'''
+        """Add the given amount to the odometer reading"""
         self.odometer_reading += miles
-
 
 class ElectricCar(Car):
 
@@ -40,4 +39,9 @@ class ElectricCar(Car):
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
 
-print("test from home")
+class Battery():
+    """A simple attempt to model a battery for an electric car"""
+
+    def __init__(self, battery_size=70):
+        """Initialise the battery's attributes"""
+        self.battery_size = battery_size
