1774a8673691614a36538e9a011e5f647aa36652
Colin <Colin_Paul@McAfee.com>
2018-11-01 08:17:53 +0000

    changes from work

--- chapter_9/electric_car.py
+++ chapter_9/electric_car.py
@@ -6,6 +6,7 @@ class Car():
         self.model = model
         self.year = year
         self.odometer_reading = 0
+        self.gas = 0
 
     def get_descriptive_name(self):
         '''Return a neatly formatted descriptive name'''
@@ -13,7 +14,10 @@ class Car():
         return long_name.title()
 
     def read_odometer(self):
-        print('Cars mileage is ' + str(self.odometer_reading))
+        print("Cars mileage is " + str(self.odometer_reading))
+
+    def gas_range(self):
+        print("Range left in tank") + str(self.gas)
 
     def update_odometer(self, mileage):
         '''
@@ -29,15 +33,30 @@ class Car():
         '''Add the given amount to the odometer reading'''
         self.odometer_reading += miles
 
+    def fill_gas_tank(self):
+        '''Add how much gas you filled the car with'''
+        print("Filling gas")
+
 
 class ElectricCar(Car):
 
     def __init__(self, make, model, year):
-        """Initialize attributes of the parent class"""
+        """
+        Initialize attributes of the parent class
+        Then Initialize attributes specific to an electric car
+        """
         super().__init__(make, model, year)
+        self.battery_size = 70
+
+    def describe_battery(self):
+        '''Print a statement describing the battery size'''
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+    def fill_gas_tank(self):
+        '''Electric cars don't have gas tanks'''
+        print("This car doesn't need a gas tank!")
 
 
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
-
-print("test from home")
+my_tesla.fill_gas_tank()
