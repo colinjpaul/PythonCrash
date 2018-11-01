1485e18db28d1d404de1e7290b10d1786e169bbe
Colin <Colin_Paul@McAfee.com>
2018-11-01 08:30:46 +0000

    changes from work

--- chapter_9/electric_car.py
+++ chapter_9/electric_car.py
@@ -9,7 +9,7 @@ class Car():
         self.gas = 0
 
     def get_descriptive_name(self):
-        '''Return a neatly formatted descriptive name'''
+        """Return a neatly formatted descriptive name"""
         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
         return long_name.title()
 
@@ -18,19 +18,20 @@ class Car():
 
     def gas_range(self):
         print("Range left in tank") + str(self.gas)
+        print('Cars mileage is ' + str(self.odometer_reading))
 
     def update_odometer(self, mileage):
-        '''
+        """
         Set the odometer reading to a given value
         Reject the change if it attempts to roll back to reading
-        '''
+        """
         if mileage >= self.odometer_reading:
             self.odometer_reading = mileage
         else:
             print("You can't roll back an odometer")
 
     def increment_odometer(self, miles):
-        '''Add the given amount to the odometer reading'''
+        """Add the given amount to the odometer reading"""
         self.odometer_reading += miles
 
     def fill_gas_tank(self):
@@ -45,6 +46,7 @@ class ElectricCar(Car):
         Initialize attributes of the parent class
         Then Initialize attributes specific to an electric car
         """
+        """Initialize attributes of the parent class"""
         super().__init__(make, model, year)
         self.battery_size = 70
 
@@ -60,3 +62,10 @@ class ElectricCar(Car):
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
 my_tesla.fill_gas_tank()
+
+class Battery():
+    """A simple attempt to model a battery for an electric car"""
+
+    def __init__(self, battery_size=70):
+        """Initialise the battery's attributes"""
+        self.battery_size = battery_size
