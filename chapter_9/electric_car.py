5d81bf3f97df2813092315c72736ef0fad66e7c6
Colin <Colin_Paul@McAfee.com>
2018-11-02 08:55:47 +0000

    adding battery class

--- chapter_9/electric_car.py
+++ chapter_9/electric_car.py
@@ -1,5 +1,5 @@
-class Car():
-    '''A sipmple attempt to represent a car'''
+class Car:
+    """A sipmple attempt to represent a car"""
 
     def __init__(self, make, model, year):
         self.make = make
@@ -35,8 +35,31 @@ class Car():
         self.odometer_reading += miles
 
     def fill_gas_tank(self):
-        '''Add how much gas you filled the car with'''
+        """Add how much gas you filled the car with"""
         print("Filling gas")
+
+
+class Battery:
+    """A simple attempt to model a battery for an electric car"""
+
+    def __init__(self, battery_size=70):
+        """Initialise the battery's attributes"""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Print a statement describing the battery size"""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+    def get_range(self):
+        """Print statement detailing the range this battery provides"""
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge"
+        print(message)
 
 
 class ElectricCar(Car):
@@ -48,24 +71,10 @@ class ElectricCar(Car):
         """
         """Initialize attributes of the parent class"""
         super().__init__(make, model, year)
-        self.battery_size = 70
-
-    def describe_battery(self):
-        '''Print a statement describing the battery size'''
-        print("This car has a " + str(self.battery_size) + "-kWh battery.")
-
-    def fill_gas_tank(self):
-        '''Electric cars don't have gas tanks'''
-        print("This car doesn't need a gas tank!")
+        self.battery = Battery()
 
 
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
-my_tesla.fill_gas_tank()
-
-class Battery():
-    """A simple attempt to model a battery for an electric car"""
-
-    def __init__(self, battery_size=70):
-        """Initialise the battery's attributes"""
-        self.battery_size = battery_size
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
