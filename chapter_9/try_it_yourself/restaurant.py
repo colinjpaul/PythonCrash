b74c981d9c5148ae9c41eef653149beb0ee2d406
Colin <Colin_Paul@McAfee.com>
2018-11-16 13:34:09 +0000

    back to this

--- chapter_9/try_it_yourself/restaurant.py
+++ chapter_9/try_it_yourself/restaurant.py
@@ -1,4 +1,4 @@
-class Restaurant():
+class Restaurant:
 
     def __init__(self, restaurant_name, cuisine_type):
         self.name = restaurant_name
@@ -20,9 +20,21 @@ class Restaurant():
         print('The Restaurant is now open')
 
 
+class IceCreamStand(Restaurant):
+    def __init__(self):
+        """Initialise attributes"""
+        self.flavors = ['Cookies & Creme', 'Chocolate', 'CheeseCake']
+
+    def display_flavors(self):
+        print('Available flavors are ' + str(self.flavors))
+
+
 restaurant = Restaurant('Conna Cuisine', 'Traditional')
 restaurant.set_number_served(17)
 restaurant.increment_number_served(7)
 
 restaurant.describe_restaurant()
 restaurant.open_restaurant()
+
+my_ice_cream_stand = IceCreamStand()
+my_ice_cream_stand.display_flavors()
