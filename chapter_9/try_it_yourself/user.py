09a07ec6fbd62854317c9cd06104d38c7b680710
colinjpaul <colinpaul@gmail.com>
2018-11-20 06:38:34 +0000

    pg178nineseven

--- chapter_9/try_it_yourself/user.py
+++ chapter_9/try_it_yourself/user.py
@@ -25,19 +25,23 @@ class User():
         print("How's it going " + self.name.title() + ' ' + self.last_name.title())
 
 
-user1 = User('colin', 'paul')
+class Admin(User):
+    def __init__(self):
+        self.privileges = ["Can add post", "Can delete post", "Can ban user"]
+
+    def show_privileges(self):
+        print("As an Admin your privileges are: ", str(self.privileges))
+
+user1 = User('Colin', 'Paul')
 
 user1.describe_user()
 user1.greet_user()
-
 user1.increment_login_attempts()
 user1.increment_login_attempts()
-user1.increment_login_attempts()
-user1.increment_login_attempts()
-
-print(user1.login_attempts)
-
 user1.reset_login_attempts()
-
+user1.increment_login_attempts()
 print(user1.login_attempts)
+
+user2 = Admin()
+user2.show_privileges()
 
