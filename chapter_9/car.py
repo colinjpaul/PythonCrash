1ba769c2b7bf26b1e31b7a77434bd7bbe22b7694
Colin <Colin_Paul@McAfee.com>
2019-01-08 13:29:34 +0000

    2 days in a row!!!

--- chapter_9/car.py
+++ chapter_9/car.py
@@ -28,3 +28,37 @@ class Car():
     def increment_odometer(self, miles):
         '''Add the given amount to the odometer reading'''
         self.odometer_reading += miles
+
+
+class Battery:
+    """A simple attempt to model a battery for an electric car"""
+
+    def __init__(self, battery_size=70):
+        """Initialise the battery's attributes"""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Print a statement describing the battery size"""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+    def get_range(self):
+        """Print statement detailing the range this battery provides"""
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge"
+        print(message)
+
+class ElectricCar(Car):
+
+    def __init__(self, make, model, year):
+        """
+        Initialize attributes of the parent class
+        Then Initialize attributes specific to an electric car
+        """
+        """Initialize attributes of the parent class"""
+        super().__init__(make, model, year)
+        self.battery = Battery()
