336eb9412b567947603369e5a0788d5e68bf0a33
Colin <Colin_Paul@McAfee.com>
2019-01-18 13:27:04 +0000

    some decent progress

--- chapter_9/electric_car_old.py
+++ chapter_9/electric_car_old.py
@@ -1,33 +1,42 @@
-class Car():
-    '''A sipmple attempt to represent a car'''
+class Car:
+    """A sipmple attempt to represent a car"""
 
     def __init__(self, make, model, year):
         self.make = make
         self.model = model
         self.year = year
         self.odometer_reading = 0
+        self.gas = 0
 
     def get_descriptive_name(self):
-        '''Return a neatly formatted descriptive name'''
+        """Return a neatly formatted descriptive name"""
         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
         return long_name.title()
 
     def read_odometer(self):
+        print("Cars mileage is " + str(self.odometer_reading))
+
+    def gas_range(self):
+        print("Range left in tank") + str(self.gas)
         print('Cars mileage is ' + str(self.odometer_reading))
 
     def update_odometer(self, mileage):
-        '''
+        """
         Set the odometer reading to a given value
         Reject the change if it attempts to roll back to reading
-        '''
+        """
         if mileage >= self.odometer_reading:
             self.odometer_reading = mileage
         else:
             print("You can't roll back an odometer")
 
     def increment_odometer(self, miles):
-        '''Add the given amount to the odometer reading'''
+        """Add the given amount to the odometer reading"""
         self.odometer_reading += miles
+
+    def fill_gas_tank(self):
+        """Add how much gas you filled the car with"""
+        print("Filling gas")
 
 
 class Battery:
@@ -52,6 +61,11 @@ class Battery:
         message += " miles on a full charge"
         print(message)
 
+    def upgrade_battery(self):
+        if self.battery_size != 85:
+            self.battery_size = 85
+
+
 class ElectricCar(Car):
 
     def __init__(self, make, model, year):
@@ -62,3 +76,12 @@ class ElectricCar(Car):
         """Initialize attributes of the parent class"""
         super().__init__(make, model, year)
         self.battery = Battery()
+
+# my_tesla = ElectricCar('tesla', 'model s', 2016)
+# print(my_tesla.get_descriptive_name())
+# my_tesla.battery.describe_battery()
+# my_tesla.battery.get_range()
+my_electric_car = ElectricCar('lada', 'model x', 1975)
+my_electric_car.battery.get_range()
+my_electric_car.battery.upgrade_battery()
+my_electric_car.battery.get_range()
--- chapter_9/my_cars.py
+++ chapter_9/my_cars.py
@@ -1,4 +1,5 @@
-from car import Car, ElectricCar
+from car import Car
+from electric_car import ElectricCar
 
 my_beetle = Car('volkswagen', 'beetle', '2016')
 print(my_beetle.get_descriptive_name())
