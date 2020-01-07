8576c11ada9d04a89864856eeb7189b115b41159
colinjpaul <colinpaul@gmail.com>
2020-01-07 22:55:20 +0000

    on the way

--- chapter_10/alice.py
+++ chapter_10/alice.py
@@ -4,3 +4,9 @@ try:
         contents = f.read()
 except FileNotFoundError:
     print(f"Sorry, the {filename} does not exist")
+else:
+    # Count the approximate number of words in the file
+    words = contents.split()
+    num_words = len(words)
+    print(f"The file {filename} has about {num_words} words")
+
